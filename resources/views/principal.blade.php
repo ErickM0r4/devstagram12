8ad5f4d39897cb370a7acf143fe9146abe1ef028
Erick Mora <m0r4.erick@gmail.com>
2026-06-12 13:30:05 -0600

    Estilos y estructura a la plantilla base

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('titulo')
-    Principal
+    Página Principal
 @endsection
 
 @section('contenido')
-    <h2 class="text-4xl font-extrabold mt-3">Hola desde plantilla principal</h2>
+    Contenido 
 @endsection
     
